0e2a7289d7cc03e9a2d1a07cb7d7174d7b0f3ea3
Aleksandr <galaktoid1995@gmail.com>
2017-01-31 23:54:27 +0300

    Rework executing SSH commands.

--- testMainClass.java
+++ testMainClass.java
@@ -1,6 +1,6 @@
 public class testMainClass {
     public static void main(String args[]) {
-        //testConnectionSSH testcon = new testConnectionSSH("root", "192.168.1.52", 22, "123456");
+        //int foo = Integer.parseInt("1234");
         new testConnectionSSH("root", "192.168.1.52", 22, "123456");
     }
 }
